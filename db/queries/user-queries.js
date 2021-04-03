f0b7eea72764bd51b60d1fae57ba4783731aa990
Mitchell Pizzacalla <mtpizzacalla@gmail.com>
2021-04-03 13:20:04 -0400

    add addUser(), deleteUser()

--- db/queries/user-queries.js
+++ db/queries/user-queries.js
@@ -18,4 +18,22 @@ function getUserByEmail(email) {
   .then(res => res.rows[0]);
 }
 
-module.exports = { getUserById, getUserByEmail };
+function addUser(email, password) {
+  return db.query(`
+    INSERT INTO users (email, password)
+    VALUES ($1, $2)
+    RETURNING *;
+  `, [email, password])
+  .then(res => res.rows[0]);
+}
+
+function deleteUser(userId) {
+  return db.query(`
+    DELETE FROM users
+    WHERE id = $1
+    RETURNING *;
+  `, [userId])
+  .then(res => res.rowCount === 1);
+}
+
+module.exports = { getUserById, getUserByEmail, addUser, deleteUser };
