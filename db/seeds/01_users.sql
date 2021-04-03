952bdaef5a882966c5ba9e1ac9b6bf7b52ffec16
Mitchell Pizzacalla <mtpizzacalla@gmail.com>
2021-04-03 11:49:46 -0400

    add users seed

---
+++ db/seeds/01_users.sql
@@ -0,0 +1,11 @@
+INSERT INTO users (email, password) VALUES
+('email1@gmail.com', '$2a$04$UGWfa5ts6prqYMR.JL1F2OkrXXXo5FPrP3DkZMySq.tNlFSJ65r8q'),
+('email2@gmail.com', '$2a$04$UGWfa5ts6prqYMR.JL1F2OkrXXXo5FPrP3DkZMySq.tNlFSJ65r8q'),
+('email3@gmail.com', '$2a$04$UGWfa5ts6prqYMR.JL1F2OkrXXXo5FPrP3DkZMySq.tNlFSJ65r8q'),
+('email4@gmail.com', '$2a$04$UGWfa5ts6prqYMR.JL1F2OkrXXXo5FPrP3DkZMySq.tNlFSJ65r8q'),
+('email5@gmail.com', '$2a$04$UGWfa5ts6prqYMR.JL1F2OkrXXXo5FPrP3DkZMySq.tNlFSJ65r8q'),
+('email6@gmail.com', '$2a$04$UGWfa5ts6prqYMR.JL1F2OkrXXXo5FPrP3DkZMySq.tNlFSJ65r8q'),
+('email7@gmail.com', '$2a$04$UGWfa5ts6prqYMR.JL1F2OkrXXXo5FPrP3DkZMySq.tNlFSJ65r8q'),
+('email8@gmail.com', '$2a$04$UGWfa5ts6prqYMR.JL1F2OkrXXXo5FPrP3DkZMySq.tNlFSJ65r8q'),
+('email9@gmail.com', '$2a$04$UGWfa5ts6prqYMR.JL1F2OkrXXXo5FPrP3DkZMySq.tNlFSJ65r8q'),
+('email10@gmail.com', '$2a$04$UGWfa5ts6prqYMR.JL1F2OkrXXXo5FPrP3DkZMySq.tNlFSJ65r8q');
