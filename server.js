87a176dba4552e7f6165e2800c5a96cdc67a368d
Mitchell Pizzacalla <mtpizzacalla@gmail.com>
2021-04-03 15:46:19 -0400

    replace bodyparser with native express function

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ require('dotenv').config();
 const PORT       = process.env.PORT || 8080;
 const ENV        = process.env.ENV || "development";
 const express    = require("express");
-const bodyParser = require("body-parser");
 const sass       = require("node-sass-middleware");
 const app        = express();
 const morgan     = require('morgan');
@@ -15,7 +14,6 @@ const cookieSession = require('cookie-session');
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan('dev'));
-
 app.use(cookieSession({
   name: 'session',
   keys: ['user_id'],
@@ -23,7 +21,7 @@ app.use(cookieSession({
 }));
 
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use("/styles", sass({
   src: __dirname + "/styles",
   dest: __dirname + "/public/styles",
