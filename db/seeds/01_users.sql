6b82b7877fb652465b0d91a9628078ac64ec4695
Mitchell Pizzacalla <mtpizzacalla@gmail.com>
2021-04-03 12:08:13 -0400

    add passwords seed

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -4,8 +4,8 @@ INSERT INTO users (email, password) VALUES
 ('LHL_employee2@gmail.com', '$2a$04$UGWfa5ts6prqYMR.JL1F2OkrXXXo5FPrP3DkZMySq.tNlFSJ65r8q'),
 ('random2@gmail.com', '$2a$04$UGWfa5ts6prqYMR.JL1F2OkrXXXo5FPrP3DkZMySq.tNlFSJ65r8q'),
 ('amazon_employee1@gmail.com', '$2a$04$UGWfa5ts6prqYMR.JL1F2OkrXXXo5FPrP3DkZMySq.tNlFSJ65r8q'),
-('random3@gmail.com', '$2a$04$UGWfa5ts6prqYMR.JL1F2OkrXXXo5FPrP3DkZMySq.tNlFSJ65r8q'),
+('amazon_employee2@gmail.com', '$2a$04$UGWfa5ts6prqYMR.JL1F2OkrXXXo5FPrP3DkZMySq.tNlFSJ65r8q'),
 ('random4@gmail.com', '$2a$04$UGWfa5ts6prqYMR.JL1F2OkrXXXo5FPrP3DkZMySq.tNlFSJ65r8q'),
-('email8@gmail.com', '$2a$04$UGWfa5ts6prqYMR.JL1F2OkrXXXo5FPrP3DkZMySq.tNlFSJ65r8q'),
-('netflix_employee2@gmail.com', '$2a$04$UGWfa5ts6prqYMR.JL1F2OkrXXXo5FPrP3DkZMySq.tNlFSJ65r8q'),
+('random5@gmail.com', '$2a$04$UGWfa5ts6prqYMR.JL1F2OkrXXXo5FPrP3DkZMySq.tNlFSJ65r8q'),
+('netflix_employee1@gmail.com', '$2a$04$UGWfa5ts6prqYMR.JL1F2OkrXXXo5FPrP3DkZMySq.tNlFSJ65r8q'),
 ('LHL_employee3@gmail.com', '$2a$04$UGWfa5ts6prqYMR.JL1F2OkrXXXo5FPrP3DkZMySq.tNlFSJ65r8q');
