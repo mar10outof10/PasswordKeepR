b515720a0db2bb97b4d9c4a10863d741dda74262
Mitchell Pizzacalla <mtpizzacalla@gmail.com>
2021-04-03 13:56:19 -0400

    add editOrg()

--- db/queries/org-queries.js
+++ db/queries/org-queries.js
@@ -9,4 +9,14 @@ function addOrg(orgName) {
   .then(res => res.rows[0]);
 }
 
-module.exports = { addOrg };
+function editOrg(orgId, newOrgName) {
+  return db.query(`
+    UPDATE orgs
+    SET name = $1
+    WHERE id = $2
+    RETURNING *;
+  `, [newOrgName, orgId])
+  .then(res => res.rows[0]);
+}
+
+module.exports = { addOrg, editOrg };
