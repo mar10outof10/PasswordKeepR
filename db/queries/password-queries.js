b9d7de3733e32963e61cc2de3b84791fab8cb1f9
Mitchell Pizzacalla <mtpizzacalla@gmail.com>
2021-04-03 13:43:44 -0400

    add addPassword()

--- db/queries/password-queries.js
+++ db/queries/password-queries.js
@@ -10,4 +10,17 @@ function getAllPasswords(userId) {
   .then(res => res.rows);
 }
 
-module.exports = { getAllPasswords };
+function addPassword(label, username, password, category, user_id, org_id) {
+
+  if (arguments.length < 6) {
+    return Promise.reject(`addPassword requires 6 arguments, only received ${arguments.length}`);
+  }
+  return db.query(`
+    INSERT INTO passwords (label, username, password, category, user_id, org_id)
+    VALUES ($1, $2, $3, $4, $5, $6)
+    RETURNING *;
+  `, Array.from(arguments))
+  .then(res => res.rows[0]);
+}
+
+module.exports = { getAllPasswords, addPassword };
