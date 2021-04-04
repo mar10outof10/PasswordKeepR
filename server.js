ae74f96793f6738752f0cbce2cf7e8c10540f770
jaredgoldman <jaredgoldmandrums@gmail.com>
2021-04-04 17:51:52 +0000

    pull user routes

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const app        = express();
 const morgan     = require('morgan');
 const cookieSession    = require('cookie-session');
 
+
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
