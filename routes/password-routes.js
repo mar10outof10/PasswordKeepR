757079e5a69572e69635183629a5803536b4e8e3
jaredgoldman <jaredgoldmandrums@gmail.com>
2021-04-04 20:12:54 +0000

    implemented middleware on all password routes

--- routes/password-routes.js
+++ routes/password-routes.js
@@ -29,98 +29,82 @@ router.get('/new', isAuthenticated, (req, res) => {
 
 // Show individual password
 
-router.get('/:id', (req, res) => {
+router.get('/:id', isAuthenticated,  (req, res) => {
   const passwordId = req.params.id;
-  if (isUserLoggedIn(req)) {
-    getPasswordById(passwordId)
-    .then(password => {
-      return res.send(password)
-      // const templateVars = { password , user: userIdCookie }
-      // return res.render('/password/:id', templateVars)
-    })
-    .catch(err => {
-      res.redirect('/login', { errorMsg: 'This password doesn\'t exist' });
-    })
-  } else {
-    res.redirect('/login', { errorMsg: 'You must be logged in to view this password' });
-  }
+  getPasswordById(passwordId)
+  .then(password => {
+    return res.send(password)
+  // const templateVars = { password , user: userIdCookie }
+  // return res.render('/password/:id', templateVars)
+  })
+  .catch(err => {
+    res.redirect('/login', { errorMsg: 'This password doesn\'t exist' });
+  })
 });
 
 
 // Add password
 
-router.post('/', (req, res) => {
-  if (isUserLoggedIn(req)) {
-    const { label, username, password, category, orgId } = req.body;
-    const newPassObj = {
-      label,
-      username,
-      password,
-      category,
-      orgId,
-      userId
-    }
-    addPassword(newPassObj)
-    .then(newPassObj => {
-      res.json(newPassObj);
-    })
-    .catch(err => {
-      res.json(err);
-    })
-  } else {
-    res.end;
+router.post('/', isAuthenticated, (req, res) => {
+  const { label, username, password, category, orgId } = req.body;
+  const newPassObj = {
+    label,
+    username,
+    password,
+    category,
+    orgId,
+    userId
   }
+  addPassword(newPassObj)
+  .then(newPassObj => {
+    res.json(newPassObj);
+  })
+  .catch(err => {
+    res.json(err);
+  })
 })
 
 
 // Edit individual password
 
-router.post('/:id', (req, res) => {
-    if (isUserLoggedIn(req)) {
-    const { label, username, password, category, orgId } = req.body;
-    const editPassObj = {
-      label,
-      username,
-      password,
-      category,
-      userId,
-      orgId,
-      passwordId
-    }
-    editPassword(editPassObj)
-    .then(editedPassObj => {
-      res.json(editedPassObj);
-    })
-    .catch(err => {
-      res.json(err);
-    })
-  } else {
-    res.end;
+router.post('/:id', isAuthenticated, (req, res) => {
+  const { label, username, password, category, orgId } = req.body;
+  const editPassObj = {
+    label,
+    username,
+    password,
+    category,
+    userId,
+    orgId,
+    passwordId
   }
+  editPassword(editPassObj)
+  .then(editedPassObj => {
+    res.json(editedPassObj);
+  })
+  .catch(err => {
+    res.json(err);
+  })
 });
 
 // Delete password
 
-router.post('/:id/delete', (req, res) => {
-  if (isUserLoggedIn(req)) {
-    const passwordId = req.params.id;
-    getPasswordById(passwordId)
-    .then(passwordObj => {
-      userIdCookie = req.params.user_id;
-      if (passwordObj.userId === userIdCookie) {
-        deletePassword(passwordId)
-        .then(rowCount => {
-          res.json(rowCount);
-          // res.redirect('/passwords');
-        })
-        .catch(err => {
-          res.json(err);
-        })
-      }
-    })
-  } else {
-    res.end;
-  }
+router.post('/:id/delete', isAuthenticated, (req, res) => {
+  const passwordId = req.params.id;
+  getPasswordById(passwordId)
+  .then(passwordObj => {
+    userIdCookie = req.params.user_id;
+    if (passwordObj.userId === userIdCookie) {
+      deletePassword(passwordId)
+      .then(rowCount => {
+        res.json(rowCount);
+        // res.redirect('/passwords');
+      })
+      .catch(err => {
+        res.json(err);
+      })
+    }
+  })
 });
 
 module.exports = router;
