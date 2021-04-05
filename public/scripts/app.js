87604ac22a7e6286b8020b27ed1eb2211866bb45
jaredgoldman <jaredgoldmandrums@gmail.com>
2021-04-05 22:16:46 +0000

    create modal display window that can be clicked in and out of

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -18,16 +18,39 @@ $(() => {
 });
 
 // Generate password window
-  // create helper function insert nev div in .modalclass
-
 
   $('.generate-password').on('click', function() {
     // click will trigger modal window to appear
+    $('.modal').css('display', 'block');
+    $('.modal-content').css('display', 'block');
+    // change modal display from none to anything
+    // generate and display password
+    // move password to password field on page
   })
+
+// While inside password window
+  // cancel out of window
+  $('.pw-cancel').on('click', function() {
+    $('.modal').css('display', 'none');
+    $('.modal-content').css('display', 'none');
+  })
+
 })
 
-const generateRandomString = (length) => {
-  const chars = '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const generateRandomString = (length, uppers, lowers, numbers) => {
+  let chars = "";
+  if (!uppers && !lowers) {
+    console.log('error');
+  }
+  if (uppers) {
+    chars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+  if (lowers) {
+    chars += "abcdefghijklmnopqrstuvwxyz";
+  }
+  if (numbers) {
+    chars += "1234567890";
+  }
   let result = '';
   for (let i = length; i > 0; --i) {
     result += chars[Math.floor(Math.random() * chars.length)];
@@ -35,6 +58,4 @@ const generateRandomString = (length) => {
   return result;
 };
 
-const createModalWindow = () => {
 
-}
