863ed6a47c40488b3fd3e01a9e5188bc88175a97
Mitchell Pizzacalla <mtpizzacalla@gmail.com>
2021-04-07 21:24:07 -0400

    turn off sass debug mode

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/styles", sass({
   src: __dirname + "/styles",
   dest: __dirname + "/public/styles",
-  debug: true,
+  debug: false,
   outputStyle: 'expanded'
 }));
 app.use(express.static("public"));
